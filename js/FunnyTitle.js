5686700b81937749a81f8397c40910d3a53275d4
16778738 <16778738@qq.com>
2020-09-22 14:49:40 +0800

    Site updated: 2020-09-22 14:49:34

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -4,12 +4,12 @@ var titleTime;
 document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
         $('[rel="icon"]').attr('href', "/funny.ico");
-        document.title = '╭(°A°`)╮ 页面崩溃啦 ~';
+        document.title = '╭(°A°`)╮ 不要离开我 ~';
         clearTimeout(titleTime);
     }
     else {
         $('[rel="icon"]').attr('href', "/favicon.ico");
-        document.title = '(ฅ>ω<*ฅ) 噫又好啦 ~' + OriginTitle;
+        document.title = '(ฅ>ω<*ฅ) 终于等到你 ~' + OriginTitle;
         titleTime = setTimeout(function () {
             document.title = OriginTitle;
         }, 2000);
